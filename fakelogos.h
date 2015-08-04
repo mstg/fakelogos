69fc1d44468cdc2e011b4a3863ee8aeb83609656
Mustafa Gezen <mustiigezen@gmail.com>
2015-08-05 01:46:30 +0200

    Better group declaration, thanks @uroboro

--- fakelogos.h
+++ fakelogos.h
@@ -6,14 +6,13 @@
 //  Copyright © 2015 Mustafa Gezen. All rights reserved.
 //
 
-#define hook(TARGET_NAME) ZKSwizzleInterface(_## TARGET_NAME,TARGET_NAME, NSObject); \
-	@implementation _## TARGET_NAME
-#define endhook @end
-#define _orig(TYPE, ...) ZKOrig(TYPE, ##__VA_ARGS__)
-#define hook_class(TARGET_NAME) ZKSwizzle(TARGET_NAME, NSStringFromClass([self class]))
-#define _def_group(x) NSMutableArray *_## x = [[NSMutableArray alloc] init];
+#define decl_group(x) NSMutableArray *_## x
 
 #define hook_group(GROUP_NAME, TARGET_NAME) \
+__attribute__((constructor (102))) void _$##GROUP_NAME##CTOR() { \
+if (! _##GROUP_NAME) _##GROUP_NAME = [[NSMutableArray alloc] init]; \
+[_##GROUP_NAME addObject:[NSString stringWithFormat:@"_$%s_%s__", # GROUP_NAME, # TARGET_NAME]]; \
+} \
 @interface _$##GROUP_NAME##_##TARGET_NAME##_ : NSObject @end \
 @implementation _$##GROUP_NAME##_##TARGET_NAME##_ \
 + (void)initialize {} \
@@ -26,11 +25,17 @@ ZKSwizzle(_$##GROUP_NAME##_##TARGET_NAME##__, TARGET_NAME); \
 @end \
 @implementation _$##GROUP_NAME##_##TARGET_NAME##__
 
-#define add_to_group(GROUP_NAME, TARGET_NAME) [_## GROUP_NAME addObject:[NSString stringWithFormat:@"_$%s_%s__", # GROUP_NAME, # TARGET_NAME]];
-
 #define init_group(x) \
-for(NSString *key in _## x){ \
-	[NSClassFromString(key) __load]; \
+for (NSString *key in _## x) { \
+[NSClassFromString(key) __load]; \
 }
 
-#define ctor __attribute__((constructor)) void init()
+#define hook(TARGET_NAME) ZKSwizzleInterface(_## TARGET_NAME,TARGET_NAME, NSObject); \
+@implementation _## TARGET_NAME
+#define endhook @end
+
+#define ctor __attribute__((constructor (103))) void init()
+
+#define hook_class(TARGET_NAME) ZKSwizzle(TARGET_NAME, NSStringFromClass([self class]))
+
+#define _orig(TYPE, ...) ZKOrig(TYPE, ##__VA_ARGS__)
